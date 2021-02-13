62fa56a3e9f4b4f35ab703f498490e4a434a36f0
hippos-man <hipposwave@gmail.com>
2021-02-13 13:58:19 +0800

    1. Created Markup for my profile page 2. Created Desktop view 3. Created Js file to add actions.

---
+++ resources/js/common.js
@@ -0,0 +1,49 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    setSeeMoreButton();
+    setCardOverray();
+    resetCardOverray();
+});
+
+function setSeeMoreButton() {
+    const seeMoreButton = document.querySelector('section#home button');
+    const aboutSection = document.getElementById('about');
+    seeMoreButton.addEventListener('click', (e) => {
+        scrollToElement(aboutSection);
+    });
+}
+
+function scrollToElement(element) {
+    element.scrollIntoView();
+}
+
+function setCardOverray() {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach((element)=>{
+        element.addEventListener('click' , (event) => {
+            let target = event.srcElement.offsetParent.children[0];
+            if (target.className == "overray"){
+                turnOnCardOverray(target);
+            }
+        });
+    });
+}
+
+function resetCardOverray() {
+    window.onclick = function(event) {
+        if (!event.srcElement.offsetParent.matches('.card')) {
+            console.log('reset!');
+            const overrays = document.querySelectorAll('.overray');
+            overrays.forEach((e) => {
+                e.style.display = "none";
+            });
+        }
+    }
+}
+
+function turnOnCardOverray(target) {
+    target.style.display = "flex";
+}
+
+function turnOffCardOveray(target) {
+    target.style.display = "none";
+}
